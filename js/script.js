a4c01712cbcf289eb5a9f89d451040656f81eff5
Renan Monteiro <renan@Renans-MacBook-Air.local>
2019-02-13 10:55:58 +0000

    Website

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
+
+  $(document).ready(function () {
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
